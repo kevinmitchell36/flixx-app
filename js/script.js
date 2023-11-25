f98f4ba09cb7c612baf40acd8a4beeb1e585b133
kevinmitchell36 <kevingm31@gmail.com>
2023-11-25 10:58:45 -0600

    Add popular shows and movie details
    Router runs tv index page and movie-details show page.

--- js/script.js
+++ js/script.js
@@ -32,14 +32,115 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="show-details.html?id=${show.id}">
+      ${
+        show.poster_path 
+          ? `<img
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}"
+        />` : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${show.first_air_date}</small>
+      </p>
+    </div>`;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = ` 
+    <div class="details-top">
+      <div>
+      ${
+        movie.poster_path 
+          ? `<img
+          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt="${movie.title}"
+        />` : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10 
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+          ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+        <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+        <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+        ${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+      </div>
+    </div>`;
+
+  document.querySelector('#movie-details').appendChild(div);
+
+}
+
+
 async function fetchAPIData(endpoint) {
   const API_KEY = '8b13a4cc510f700167489b1bdaca18e1';
   const API_URL = 'https://api.themoviedb.org/3/';
-  const response = await fetch(`${API_URL}${endpoint}?api_key=8b13a4cc510f700167489b1bdaca18e1`)
+  
+  showSpinner();
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=8b13a4cc510f700167489b1bdaca18e1`);
   const data = await response.json();
+
+  hideSpinner();
+
   return data; 
 }
 
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
 
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -48,6 +149,10 @@ function highlightActiveLink() {
       link.classList.add('active');
     }
   })
+}
+
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 
@@ -59,10 +164,10 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
