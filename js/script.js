87f09a7ba4f2aa3a894f55f7a81e8a05ac221291
kevinmitchell36 <kevingm31@gmail.com>
2023-11-26 20:33:18 -0600

    Add backgrounds/show details/swiper
    Can view the tv show page, home page has now playing carousel, all pages display muted background overlay

--- js/script.js
+++ js/script.js
@@ -67,6 +67,8 @@ async function displayMovieDetails() {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = ` 
     <div class="details-top">
@@ -119,6 +121,128 @@ async function displayMovieDetails() {
 
 }
 
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+console.log(show);
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = ` 
+    <div class="details-top">
+      <div>
+      ${
+        show.poster_path 
+          ? `<img
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}"
+        />` : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+      }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)} / 10 
+        </p>
+        <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+        <p>
+          ${show.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+        <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}</li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+        ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+      </div>
+    </div>`;
+
+  document.querySelector('#show-details').appendChild(div);
+
+}
+
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
+
+async function displaySlider() {
+  const {results}   = await fetchAPIData('movie/now_playing')
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}
+        "alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+    initSwiper();
+  })
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freemode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000, 
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  })
+}
 
 async function fetchAPIData(endpoint) {
   const API_KEY = '8b13a4cc510f700167489b1bdaca18e1';
@@ -161,6 +285,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':  
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
@@ -169,8 +294,8 @@ function init() {
     case '/movie-details.html':
       displayMovieDetails();
       break;
-    case '/tv-details.html':
-      console.log('TV Details');
+    case '/show-details.html':
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
