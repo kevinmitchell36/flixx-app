1244fb2df486f386485a7e8a6e1994b13f48b295
kevinmitchell36 <kevingm31@gmail.com>
2023-11-29 10:46:01 -0600

    Finish search options/working pagination
    Search terms return results. Results separated by pagination

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
     term: '',
     type: '',
     page: 1,
-    totalPages: 1
+    totalPages: 1,
+    totalResults: 0
   },
   api: {
     apiKey: '8b13a4cc510f700167489b1bdaca18e1',
@@ -217,11 +218,90 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const results = await searchAPIData();
-    console.log(results);
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+    displaySearchResults(results);
+    document.querySelector('#search-term').value = '';
   } else {
     showAlert('Please enter a search term');
   }
+}
+
+function displaySearchResults(results) {
+  window.scrollTo(0, 0);
+  document.querySelector('#search-results').innerHTML= '';
+  document.querySelector('#search-results-heading').innerHTML= '';
+  document.querySelector('#pagination').innerHTML= '';
+
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="${global.search.type}-details.html?id=${result.id}">
+      ${
+        result.poster_path 
+          ? `<img
+          src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+          class="card-img-top"
+          alt="${global.search.type === 'movie' ? result.title : result.name}"
+        />` : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${global.search.type === 'movie' ? result.title : result.name}"
+      />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+      </p>
+    </div>`;
+    document.querySelector('#search-results-heading').innerHTML = `<h2>${results.length} of ${global.search.totalResults}</h2>`
+    document.querySelector('#search-results').appendChild(div);
+  });
+
+  displayPagination();
+}
+
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages} </div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  })
+
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  })
 }
 
 async function displaySlider() {
@@ -288,7 +368,7 @@ async function searchAPIData() {
   
   showSpinner();
 
-  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
   const data = await response.json();
 
   hideSpinner();
@@ -313,7 +393,7 @@ function highlightActiveLink() {
   })
 }
 
-function showAlert(message, className) {
+function showAlert(message, className = 'error') {
   const alertEl = document.createElement('div');
   alertEl.classList.add('alert', className);
   alertEl.appendChild(document.createTextNode(message));
